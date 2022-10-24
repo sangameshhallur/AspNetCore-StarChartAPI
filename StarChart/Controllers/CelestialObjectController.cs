9f7ff3abbeb32cbbe48cb88d2218008c8627818f
Sangamesh Hallur <shallur@optum.com>
2022-10-24 22:01:05 +0530

    Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -8,10 +11,41 @@ namespace StarChart.Controllers
     public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-
+        CelestialObject celestialObject = new CelestialObject();
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {            
+            if (_context.CelestialObjects.Contains(id))
+            {
+                return NotFound();
+            }
+
+            celestialObject.Satellites = _context.CelestialObjects.Where(a => a.Id.Equals(id)).ToList();
+            return Ok(celestialObject.Satellites);
+
+        }
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            if (_context.CelestialObjects.Contains(name))
+            {
+                return NotFound();
+            }
+
+            celestialObject.Satellites = _context.CelestialObjects.Where(a=> a.Name.Equals(name)).ToList();
+            return Ok(celestialObject.Satellites);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            celestialObject.Satellites = _context.CelestialObjects.ToList();
+            return Ok(celestialObject.Satellites);
         }
     }
 }
