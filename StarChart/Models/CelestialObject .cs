96a7cdd2c91d885b7b08d916a884275a2637882a
Sangamesh Hallur <shallur@optum.com>
2022-10-24 19:56:59 +0530

    Create CelestialObject Model

---
+++ StarChart/Models/CelestialObject .cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int? OrbitedObjectId { get; set; }
+        [NotMapped]
+        public List<CelestialObject> Satellites { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
+    }
+}
